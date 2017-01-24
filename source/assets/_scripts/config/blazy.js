e5a254779982514738a12ce8094ba8aaa49bfe10
S1SYPHOS <m.folkers@posteo.de>
2017-01-24 11:55:28 +0100

    Added srcset functionality

--- source/assets/_scripts/config/blazy.js
+++ source/assets/_scripts/config/blazy.js
@@ -10,17 +10,8 @@ var lazyload = new Blazy({
   selector: '.lazy',
   successClass: 'lazy--has-loaded',
   errorClass: 'lazy--has-error',
-  // breakpoints: [
-  //   {
-  //     width: 480,
-  //     src: 'data-src-480'
-  //   }
-  // ],
   success: function(element){
     setTimeout(function(){
-      // We want to remove the loader gif now.
-      // First we find the parent container
-      // then we remove the "loading" class which holds the loader image
       var parent = element.parentNode;
       parent.className = parent.className.replace(/\bloading\b/, 'loaded');
     }, 200);
